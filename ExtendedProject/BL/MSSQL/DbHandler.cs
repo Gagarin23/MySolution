481b24c678ca322a83672eafed7f72a502da78ac
Gagarin23 <pedoclaus992@gmail.com>
2020-11-08 21:19:04 +0300

    Удалил удаление.

--- ExtendedProject/BL/MSSQL/DbHandler.cs
+++ ExtendedProject/BL/MSSQL/DbHandler.cs
@@ -30,24 +30,5 @@ namespace ExtendedProject.BL.MSSQL
                 db.SaveChanges();
             }
         }
-
-        public void DeleteOffer(Shop shop)
-        {
-            using (var db = new TestDbContext())
-            {
-                db.Database.OpenConnection();
-                db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT dbo.Offers ON;");
-
-                var foundedShop = db.Shops.Where(s => s.ShopId == shop.ShopId).AsNoTracking();
-
-                if (foundedShop.SingleOrDefault() == null)
-                    return;
-
-                var offers = db.Offers.Where(o => o.OfferId == shop.Offers.First().OfferId);
-                var shops = offers.Select(o => o.Shops);
-
-                db.SaveChanges();
-            }
-        }
     }
 }
